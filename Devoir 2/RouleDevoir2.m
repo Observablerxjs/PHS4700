2a9d2b41af87a09b68ea2ead609926df3439b9ce
Yacine Benouniche <benounicheyacine@gmail.com>
2019-10-15 16:04:48 -0400

    mise a jour fichier rouleDevoir

--- Devoir 2/RouleDevoir2.m
+++ Devoir 2/RouleDevoir2.m
@@ -15,7 +15,6 @@ for vit=1:3
 %
 %  Tracer terrain et position de la balle de golf 
 %
-
   fprintf('\nSimulation %3d\n',vit);
   for option=1:3
     if option == 1
@@ -31,15 +30,10 @@ for vit=1:3
     fprintf('Position initiale de la balle [%12.8f %12.8f]  m \n',xy0(1),xy0(2));
     fprintf('Vitesse initiale de la balle  [%12.8f %12.8f %12.8f]  m/s \n',vb0(1),vb0(2),vb0(3));
     fprintf('Vitesse angulaire de la balle [%12.8f %12.8f %12.8f] rad/s \n',wb0(1),wb0(2),wb0(3));
-    %% IMPORTANT !!!!!: NE PAS OUBLIE DE RECHANGER CETTE LIGNE %%
-    %%[coupref vfref tref rbtref ]=Devoir2ref(option,xy0,vb0,wb0);%%
-    [coupref vfref tref rbtref ]=Devoir2(option,xy0,vb0,wb0);
-    %fprintf('**************************************************** \n')
     [coup vf t rbt ]=Devoir2(option,xy0,vb0,wb0);
-    %fprintf('**************************************************** \n')
     lastt=length(t);
-    fprintf('\nCoup %3d Delta Ref %3d \n',coup,coup-coupref);
-    fprintf('\nLa simualtion se termine au temps %12.8f s Delta Ref %12.8f s \n',t(lastt),t(lastt)-tref(lastt));
+    fprintf('\nCoup %3d \n',coup);
+    fprintf('\nLa simualtion se termine au temps %12.8f s \n',t(lastt));
     if coup == 0
       fprintf('Le golfeur a un trou d''un coup \n');
     elseif coup == 1
@@ -50,9 +44,7 @@ for vit=1:3
       fprintf('Le balle sort du terrain \n');
     end
     fprintf('Vitesse finale de la balle     [%12.8f %12.8f %12.8f]  m/s\n',vf(1),vf(2),vf(3));
-    fprintf('Delta Ref                      [%12.8f %12.8f %12.8f]  m/s\n',vf(1)-vfref(1),vf(2)-vfref(2),vf(3)-vfref(3));
     fprintf('Position finale de la balle    [%12.8f %12.8f %12.8f]  m \n',rbt(lastt,1),rbt(lastt,2),rbt(lastt,3));
-    fprintf('Delta Ref                      [%12.8f %12.8f %12.8f]  m \n',rbt(lastt,1)-rbtref(lastt,1),rbt(lastt,2)-rbtref(lastt,2),rbt(lastt,3)-rbtref(lastt,3));
     fprintf('\n\n');
   end
   pause;
