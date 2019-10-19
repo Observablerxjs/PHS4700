08a9a97ff75c1c3aa357923a91f926f8618d6a6c
Yacine Benouniche <benounicheyacine@gmail.com>
2019-10-19 12:22:51 -0400

    devoir2

--- Devoir 2/RouleDevoir2.m
+++ Devoir 2/RouleDevoir2.m
@@ -16,7 +16,14 @@ for vit=1:3
 %  Tracer terrain et position de la balle de golf 
 %
   fprintf('\nSimulation %3d\n',vit);
+  
+## section pour dessiner graphique
+##  rbt_t = cell (3,1);
+  
+  file_id = fopen(strcat('Simulation ',num2str(vit),'.txt'), 'w');
+  
   for option=1:3
+  
     if option == 1
       fprintf('Acceleration gravitationnelle seulement \n');
       type='r-';
@@ -31,6 +38,7 @@ for vit=1:3
     fprintf('Vitesse initiale de la balle  [%12.8f %12.8f %12.8f]  m/s \n',vb0(1),vb0(2),vb0(3));
     fprintf('Vitesse angulaire de la balle [%12.8f %12.8f %12.8f] rad/s \n',wb0(1),wb0(2),wb0(3));
     [coup vf t rbt ]=Devoir2(option,xy0,vb0,wb0);
+    
     lastt=length(t);
     fprintf('\nCoup %3d \n',coup);
     fprintf('\nLa simualtion se termine au temps %12.8f s \n',t(lastt));
@@ -46,6 +54,22 @@ for vit=1:3
     fprintf('Vitesse finale de la balle     [%12.8f %12.8f %12.8f]  m/s\n',vf(1),vf(2),vf(3));
     fprintf('Position finale de la balle    [%12.8f %12.8f %12.8f]  m \n',rbt(lastt,1),rbt(lastt,2),rbt(lastt,3));
     fprintf('\n\n');
+
+## section pour dessiner graphique
+##    rbt_t {option} = rbt
+    
+## section pour ecrire resultats dans fichier txt
+##    fdisp(file_id, strcat('Option ',num2str(option)));
+##    fdisp(file_id, t(lastt));
+##    fdisp(file_id, [vf(1),vf(2),vf(3)]);
+##    fdisp(file_id, [rbt(lastt,1),rbt(lastt,2),rbt(lastt,3)]);
+##    fdisp(file_id, coup);
+    
   end
-  pause;
+## section pour ecrire resultats dans fichier txt
+##    fclose(file_id);
+    
+## section pour dessiner graphique
+##    PlotGraph(rbt_t{1},rbt_t{2},rbt_t{3},strcat('Simulation ',num2str(vit)));
+ 
 end
