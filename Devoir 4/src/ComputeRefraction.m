056228e759605c765a96d232ab35caabd3172982
Yacine Benouniche <benounicheyacine@gmail.com>
2019-11-24 21:11:41 -0500

    CODE DONE

--- Devoir 4/src/ComputeRefraction.m
+++ Devoir 4/src/ComputeRefraction.m
@@ -1,2 +1,2 @@
-function [rayonVec] = ComputeRefraction(rayonInital, normale, theta1, theta2)        A = (rayonInital + cos(theta1)*normale)/sin(theta1);        B = -normale * cos(theta2);        # rayon réfracté        rayonVec = A + B;
+function [rayonVec] = ComputeRefraction(rayonInital, normale, theta1, theta2)        epsilon = 0.000001;        if (sin(theta1) - sin(theta2)) > epsilon          A = ((rayonInital + cos(theta1)*normale)/sin(theta1))*sin(theta2);          B = -normale * cos(theta2);          # rayon réfracté          rayonVec = A + B;        else          rayonVec = rayonInital;        end
 endfunction
